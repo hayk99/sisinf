156750053ae3b58f5f5b5182cf04073e38d05842
Hayk <hayk@hayk.pc>
2019-10-29 16:18:19 +0100

    subo gestor de conexiones y algunas modificaciones de ficheros varios

---
+++ ConnectionManager.java
@@ -0,0 +1,34 @@
+package es.unizar.sising.data.db;
+import java.sql*;
+
+import javax.naming.Context;
+import.naming.InitialContxt;
+import javax.sql.DataSource;
+
+/*
+* Ckase que abstrae la conexion con la base de datos
+* Hayk Kocharyan - Javier Salamero - Jesus Villacampa
+*/
+public class ConnectioManager {
+	// Devuelve la conexion, para no tener que abrirla y cerrar siempre
+	public final static Connection getConnection() throws SQLException {
+		try {
+			Context initCtx = new InitialContxt();
+			Context envCtx = (Contex) initCtx.lookup("java:comp/env");
+			System.out.println(envCtx.toString());
+			DataSource ds = (DataSource)envCtx.lookup("jdbc/sisinfDB");
+			System.out.println(ds.toString());
+
+			Connection conn = ds.getConnection();
+			return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	//Libera la conexión, devolviendo al pool
+	public final static void releaseConnection(Connection conn) throws SQLException {
+		conn.close();
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sisinf
+
+METER context.xml y server.xml en la carpeta de /docker/tomcat/
+	ahi tenemos configurado las configuraciones de las conexiones y los alias
