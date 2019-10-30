c16063815989cd617ef32f8478a0cb2552037c68
Hayk <hayk@hayk.pc>
2019-10-30 19:39:39 +0100

    subo usuariodao y conection manager

--- sisinf/src/es/unizar/sisinf/data/dao/UsuarioDAO.java
+++ sisinf/src/es/unizar/sisinf/data/dao/UsuarioDAO.java
@@ -1,5 +1,55 @@
 package es.unizar.sisinf.data.dao;
 
-public class UsuarioDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import es.unizar.sisinf.data.db.ConnectionManager;
+import es.unizar.sisinf.data.vo.UsuarioVO;
+
+/**
+ * Clse DAO de gestión de objetos usuario
+ * @author hayk
+ *
+ */
+public class UsuarioDAO {
+	private static String findByIdQuery = "SELECT * FROM usuario WHERE id = ?";
+	private static String findAll = "SELECT * FROM usuario";
+	
+	/**
+	 * Busca un registrod en la tabla DEMO por ID
+	 * @param id Identificador del regsitro buscado
+	 * @return Objeto DemoVO con el identificador buscado, o null si no se encuentra
+	 */
+	public UsuarioVO findById (int id) {
+		
+		UsuarioVO result = null;
+		
+		try {
+			//Abrimos la conexión e inicializamos los parámetros
+			Connection conn = ConnectionManager.getConnection();
+			PreparedStatement ps = conn.prepareStatement(findByIdQuery);
+			ps.setInt(1,id);
+			
+			//Ejecutamos la consulta
+			ResultSet rs = ps.executeQuery();
+			
+			//Leemos resultados
+			if (rs.first()) {
+				result = new UsuarioVO(rs.getInt("id"), rs.getString("name"));
+			}
+			else {
+				result = null;
+			}
+			ConnectionManager.releaseConnection(conn);
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return result;
+	}
 }
--- sisinf/src/es/unizar/sisinf/data/db/ConnectionManager/ConnectionManager.java
+++ sisinf/src/es/unizar/sisinf/data/db/ConnectionManager/ConnectionManager.java
@@ -3,19 +3,19 @@
 * Hayk Kocharyan - Javier Salamero - Jesus Villacampa
 */
 
-package es.unizar.sising.data.db;
-import java.sql*;
+package es.unizar.sisinf.data.db.ConnectionManager;
+import java.sql.*;
 
 import javax.naming.Context;
-import javax.naming.InitialContxt;
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-public class ConnectioManager {
+public class ConnectionManager {
 	// Devuelve la conexion, para no tener que abrirla y cerrar siempre
 	public final static Connection getConnection() throws SQLException {
 		try {
-			Context initCtx = new InitialContxt();
-			Context envCtx = (Contex) initCtx.lookup("java:comp/env");
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
 			System.out.println(envCtx.toString());
 			DataSource ds = (DataSource)envCtx.lookup("jdbc/sisinfDB");
 			System.out.println(ds.toString());
